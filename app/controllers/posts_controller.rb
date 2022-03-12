d4b8020418b4f01f8e8b5da237b8e1c33bd5c1e0
binarygit <inscription4@outlook.com>
2022-03-12 14:13:29 +0545

    move partial and add func. to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(form_params)
+    if @post.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def form_params 
+    params.require(:post).permit(:title, :body)
+  end
 end
